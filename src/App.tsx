4ab3d8b2d715989e3675e01b610b0efbe15455d1
JuanTorresJunior <torres.juand1018@gmail.com>
2022-11-12 19:39:06 -0500

    Creacion de

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { FC, useState } from 'react'
 
-function App() {
+
+const App:FC = () => {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <header className="header-app">
+        <h1>Contador en React!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non posuere est. Aliquam rhoncus porttitor lacus, sit amet dictum sem rutrum sed. Phasellus ac velit eleifend, dictum justo eu, malesuada erat.</p>
+      </header>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
